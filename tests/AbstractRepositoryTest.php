a1eb949175df5c92bdba708e9c2d65fe387eab54
tarasmoroz <taras.moroz@ekreative.com>
2023-09-21 12:09:45 +0200

    Added testFindBooksByCategoryId

---
+++ tests/AbstractRepositoryTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+abstract class AbstractRepositoryTest extends KernelTestCase
+{
+    protected ?EntityManagerInterface $em;
+
+    // Run before every test
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->em = self::getContainer()->get('doctrine.orm.entity_manager');
+    }
+
+    protected function getRepositoryForEntity(string $entityClass): mixed
+    {
+        return $this->em->getRepository($entityClass);
+    }
+
+    // Run after every test
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->em->close();
+        $this->em = null;
+    }
+}
