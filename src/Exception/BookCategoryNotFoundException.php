0bfeeda880187b1ff381103f4ae862932062004a
tarasmoroz <taras.moroz@ekreative.com>
2023-09-22 13:10:01 +0200

    Added Api Exception for Book Category.

--- src/Exception/BookCategoryNotFoundException.php
+++ src/Exception/BookCategoryNotFoundException.php
@@ -2,12 +2,10 @@
 
 namespace App\Exception;
 
-use Symfony\Component\HttpFoundation\Response;
-
 class BookCategoryNotFoundException extends \RuntimeException
 {
     public function __construct()
     {
-        parent::__construct('Book category not found', Response::HTTP_NOT_FOUND);
+        parent::__construct('Book category not found');
     }
 }
