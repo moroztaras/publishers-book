bc9634de232550c6c3dbf1885f7e249621fc27e3
tarasmoroz <taras.moroz@ekreative.com>
2023-09-28 15:53:07 +0200

    added testResolvesClassItself

--- tests/Manager/ExceptionHandler/ExceptionMappingResolverTest.php
+++ tests/Manager/ExceptionHandler/ExceptionMappingResolverTest.php
@@ -5,6 +5,8 @@ namespace App\Tests\Manager\ExceptionHandler;
 use App\Manager\ExceptionHandler\ExceptionMappingResolver;
 use App\Tests\AbstractTestCase;
 use InvalidArgumentException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ExceptionMappingResolverTest extends AbstractTestCase
 {
@@ -23,5 +25,19 @@ class ExceptionMappingResolverTest extends AbstractTestCase
         $resolver = new ExceptionMappingResolver([]);
 
         $this->assertNull($resolver->resolve(\InvalidArgumentException::class));
+    }
+
+    // When can find a class that matches what pass.
+    public function testResolvesClassItself(): void
+    {
+        // Create resolver
+        $resolver = new ExceptionMappingResolver([InvalidArgumentException::class => ['code' => Response::HTTP_BAD_REQUEST]]);
+        // To resolve
+        $mapping = $resolver->resolve(InvalidArgumentException::class);
+
+        // Comparing the actual returned value with the expected value.
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $mapping->getCode());
+        $this->assertTrue($mapping->isHidden());
+        $this->assertFalse($mapping->isLoggable());
     }
 }
