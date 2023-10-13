a8f820ec07d180abd31f54afae5054a9c46b48eb
tarasmoroz <taras.moroz@ekreative.com>
2023-10-13 12:55:24 +0200

    Added testResolveThrowsWhenDeserialize

--- tests/ArgumentResolver/RequestBodyArgumentResolverTest.php
+++ tests/ArgumentResolver/RequestBodyArgumentResolverTest.php
@@ -4,9 +4,11 @@ namespace App\Tests\ArgumentResolver;
 
 use App\ArgumentResolver\RequestBodyArgumentResolver;
 use App\Attribute\RequestBody;
+use App\Exception\RequestBodyConvertException;
 use App\Tests\AbstractTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -45,6 +47,29 @@ class RequestBodyArgumentResolverTest extends AbstractTestCase
 
         // Comparing the actual returned response with the expected metadata.
         $this->assertFalse($this->createResolver()->supports(new Request(), $meta));
+    }
+
+    public function testResolveThrowsWhenDeserialize(): void
+    {
+        // Expect exception
+        $this->expectException(RequestBodyConvertException::class);
+
+        // Create request
+        $request = new Request([], [], [], [], [], [], 'testing content');
+
+        // Create meta data
+        $meta = new ArgumentMetadata('some', \stdClass::class, false, false, null, false, [
+            new RequestBody(),
+        ]);
+
+        // Set the behavior of the method deserialize
+        $this->serializer->expects($this->once())
+            ->method('deserialize')
+            ->with('testing content', \stdClass::class, JsonEncoder::FORMAT)
+            ->willThrowException(new \Exception());
+
+        // Run method resolve
+        $this->createResolver()->resolve($request, $meta)->next();
     }
 
     // Helper method for create resolver
