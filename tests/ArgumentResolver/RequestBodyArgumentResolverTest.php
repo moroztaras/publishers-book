9bd8f8316bbb4cc5d7857d4199dfa118206667cf
tarasmoroz <taras.moroz@ekreative.com>
2023-10-13 13:26:00 +0200

    Added testResolveThrowsWhenValidationFails

--- tests/ArgumentResolver/RequestBodyArgumentResolverTest.php
+++ tests/ArgumentResolver/RequestBodyArgumentResolverTest.php
@@ -5,11 +5,14 @@ namespace App\Tests\ArgumentResolver;
 use App\ArgumentResolver\RequestBodyArgumentResolver;
 use App\Attribute\RequestBody;
 use App\Exception\RequestBodyConvertException;
+use App\Exception\ValidationException;
 use App\Tests\AbstractTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class RequestBodyArgumentResolverTest extends AbstractTestCase
@@ -67,6 +70,39 @@ class RequestBodyArgumentResolverTest extends AbstractTestCase
             ->method('deserialize')
             ->with('testing content', \stdClass::class, JsonEncoder::FORMAT)
             ->willThrowException(new \Exception());
+
+        // Run method resolve
+        $this->createResolver()->resolve($request, $meta)->next();
+    }
+
+    public function testResolveThrowsWhenValidationFails(): void
+    {
+        // Expect Validation exception
+        $this->expectException(ValidationException::class);
+
+        $body = ['test' => true];
+        $encodedBody = json_encode($body);
+
+        // Create request
+        $request = new Request([], [], [], [], [], [], $encodedBody);
+        // Create meta data
+        $meta = new ArgumentMetadata('some', \stdClass::class, false, false, null, false, [
+            new RequestBody(),
+        ]);
+
+        // Set the behavior of the method deserialize
+        $this->serializer->expects($this->once())
+            ->method('deserialize')
+            ->with($encodedBody, \stdClass::class, JsonEncoder::FORMAT)
+            ->willReturn($body);
+
+        // Set the behavior of the method validate
+        $this->validator->expects($this->once())
+            ->method('validate')
+            ->with($body)
+            ->willReturn(new ConstraintViolationList([
+                new ConstraintViolation('error', null, [], null, 'some', null),
+            ]));
 
         // Run method resolve
         $this->createResolver()->resolve($request, $meta)->next();
