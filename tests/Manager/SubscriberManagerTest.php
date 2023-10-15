c1c80dd4694569afe0dff82773c5b5f2e511931c
tarasmoroz <taras.moroz@ekreative.com>
2023-10-15 18:54:23 +0200

    Added testSubscribe

--- tests/Manager/SubscriberManagerTest.php
+++ tests/Manager/SubscriberManagerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Manager;
 
+use App\Entity\Subscriber;
 use App\Exception\SubscriberAlreadyExistsException;
 use App\Manager\SubscriberManager;
 use App\Model\SubscriberRequest;
@@ -35,13 +36,39 @@ class SubscriberManagerTest extends AbstractTestCase
         $this->repository->expects($this->once())
             ->method('existsByEmail')
             ->with(self::EMAIL)
-            ->willReturn(true)
-        ;
+            ->willReturn(true);
 
         $request = new SubscriberRequest();
         $request->setEmail(self::EMAIL);
 
         // Run method subscribe
+        (new SubscriberManager($this->repository, $this->em))->subscribe($request);
+    }
+
+    public function testSubscribe(): void
+    {
+        // Set behavior for existsByEmail method
+        $this->repository->expects($this->once())
+            ->method('existsByEmail')
+            ->with(self::EMAIL)
+            ->willReturn(false);
+
+        $expectedSubscriber = new Subscriber();
+        $expectedSubscriber->setEmail(self::EMAIL);
+
+        // Set behavior for Entity Manager
+        $this->em->expects($this->once())
+            ->method('persist')
+            ->with($expectedSubscriber);
+
+        $this->em->expects($this->once())
+            ->method('flush');
+
+        // Create request
+        $request = new SubscriberRequest();
+        $request->setEmail(self::EMAIL);
+
+        // Create subscribe manager and run method
         (new SubscriberManager($this->repository, $this->em))->subscribe($request);
     }
 }
