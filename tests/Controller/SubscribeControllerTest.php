ad2874d33c6234bfc385002d5d30360a0d6ff12e
tarasmoroz <taras.moroz@ekreative.com>
2023-10-15 20:39:31 +0200

    Added testSubscribeNotAgreed

--- tests/Controller/SubscribeControllerTest.php
+++ tests/Controller/SubscribeControllerTest.php
@@ -8,10 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SubscribeControllerTest extends AbstractControllerTest
 {
+    public const EMAIL = 'test@test.com';
+
     // Tests for successful
     public function testSubscribe(): void
     {
-        $content = json_encode(['email' => 'test@test.com', 'agreed' => true]);
+        $content = json_encode(['email' => self::EMAIL, 'agreed' => true]);
         $this->client->request(Request::METHOD_POST, '/api/v1/subscribe', [], [], [], $content);
 
         // Response was successful
@@ -19,5 +21,21 @@ class SubscribeControllerTest extends AbstractControllerTest
 
         // Comparing the expected value with the actual returned value.
         $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testSubscribeNotAgreed(): void
+    {
+        $content = json_encode(['email' => self::EMAIL]);
+        $this->client->request(Request::METHOD_POST, '/api/v1/subscribe', [], [], [], $content);
+        $responseContent = json_decode($this->client->getResponse()->getContent());
+
+        // Check status code
+        $this->assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
+        // Comparing the actual response content with the expected schema.
+        $this->assertJsonDocumentMatches($responseContent, [
+            '$.message' => 'Validation failed',
+            '$.details.violations' => self::countOf(1),
+            '$.details.violations[0].field' => 'agreed',
+        ]);
     }
 }
