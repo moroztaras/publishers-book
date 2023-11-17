ce164e706e694eac2bfcb92fc12c6d53ce57c977
tarasmoroz <taras.moroz@ekreative.com>
2023-11-17 12:36:57 +0100

    Testing AdminController

--- tests/AbstractControllerTest.php
+++ tests/AbstractControllerTest.php
@@ -2,10 +2,13 @@
 
 namespace App\Tests;
 
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Helmich\JsonAssert\JsonAssertions;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 abstract class AbstractControllerTest extends WebTestCase
 {
@@ -15,12 +18,15 @@ abstract class AbstractControllerTest extends WebTestCase
 
     protected ?EntityManagerInterface $em;
 
+    protected UserPasswordHasherInterface $hasher;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->client = static::createClient();
         $this->em = self::getContainer()->get('doctrine.orm.entity_manager');
+        $this->hasher = self::getContainer()->get('security.user_password_hasher');
     }
 
     protected function tearDown(): void
@@ -29,5 +35,55 @@ abstract class AbstractControllerTest extends WebTestCase
 
         $this->em->close();
         $this->em = null;
+    }
+
+    // Sing in user
+    protected function auth(string $username, string $password): void
+    {
+        $this->client->request(
+            Request::METHOD_POST,
+            '/api/v1/auth/login',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode(['username' => $username, 'password' => $password])
+        );
+
+        $this->assertResponseIsSuccessful();
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $data['token']));
+    }
+
+    protected function createUser(string $username, string $password): User
+    {
+        return $this->createUserWithRoles($username, $password, ['ROLE_USER']);
+    }
+
+    protected function createAdmin(string $username, string $password): User
+    {
+        return $this->createUserWithRoles($username, $password, ['ROLE_ADMIN']);
+    }
+
+    protected function createAuthor(string $username, string $password): User
+    {
+        return $this->createUserWithRoles($username, $password, ['ROLE_AUTHOR']);
+    }
+
+    private function createUserWithRoles(string $username, string $password, array $roles): User
+    {
+        $user = (new User())
+            ->setRoles($roles)
+            ->setLastName($username)
+            ->setFirstName($username)
+            ->setEmail($username)
+        ;
+
+        $user->setPassword($this->hasher->hashPassword($user, $password));
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        return $user;
     }
 }
