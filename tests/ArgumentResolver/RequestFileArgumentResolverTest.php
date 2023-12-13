dedf106ff19e1e728159302e056c81c964d68f54
tarasmoroz <taras.moroz@ekreative.com>
2023-12-13 14:46:16 +0100

    Added testResolveThrowsWhenConstraintFails

--- tests/ArgumentResolver/RequestFileArgumentResolverTest.php
+++ tests/ArgumentResolver/RequestFileArgumentResolverTest.php
@@ -10,6 +10,7 @@ use stdClass;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\Validator\Constraints\NotNull;
 use Symfony\Component\Validator\ConstraintViolation;
 use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -58,6 +59,30 @@ class RequestFileArgumentResolverTest extends AbstractTestCase
         $this->validator->expects($this->once())
             ->method('validate')
             ->with($file, [])
+            ->willReturn(new ConstraintViolationList([
+                new ConstraintViolation('error', null, [], null, 'some', null),
+            ]));
+
+        $this->createResolver()->resolve($request, $meta)->next();
+    }
+
+    public function testResolveThrowsWhenConstraintFails(): void
+    {
+        $this->expectException(ValidationException::class);
+
+        $constraints = [new NotNull()];
+        $file = new UploadedFile('path', 'field', null, UPLOAD_ERR_NO_FILE, true);
+        $request = new Request();
+        $request->files->add(['field' => $file]);
+
+        $meta = new ArgumentMetadata('some', stdClass::class, false, false, null, false, [
+            new RequestFile('field', $constraints),
+        ]);
+
+        // Set the behavior of the method validate
+        $this->validator->expects($this->once())
+            ->method('validate')
+            ->with($file, $constraints)
             ->willReturn(new ConstraintViolationList([
                 new ConstraintViolation('error', null, [], null, 'some', null),
             ]));
