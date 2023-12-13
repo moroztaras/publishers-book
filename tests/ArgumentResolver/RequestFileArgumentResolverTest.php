019d741bd0be43a0f73c0fac931cb4f84c96ca77
tarasmoroz <taras.moroz@ekreative.com>
2023-12-13 14:44:17 +0100

    Added testResolveThrowsWhenValidationFails

--- tests/ArgumentResolver/RequestFileArgumentResolverTest.php
+++ tests/ArgumentResolver/RequestFileArgumentResolverTest.php
@@ -4,9 +4,14 @@ namespace App\Tests\ArgumentResolver;
 
 use App\ArgumentResolver\RequestFileArgumentResolver;
 use App\Attribute\RequestFile;
+use App\Exception\ValidationException;
 use App\Tests\AbstractTestCase;
+use stdClass;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class RequestFileArgumentResolverTest extends AbstractTestCase
@@ -35,6 +40,29 @@ class RequestFileArgumentResolverTest extends AbstractTestCase
         $meta = new ArgumentMetadata('some', null, false, false, null);
 
         $this->assertFalse($this->createResolver()->supports(new Request(), $meta));
+    }
+
+    public function testResolveThrowsWhenValidationFails(): void
+    {
+        $this->expectException(ValidationException::class);
+
+        $file = new UploadedFile('path', 'field', null, UPLOAD_ERR_NO_FILE, true);
+        $request = new Request();
+        $request->files->add(['field' => $file]);
+
+        $meta = new ArgumentMetadata('some', stdClass::class, false, false, null, false, [
+            new RequestFile('field', []),
+        ]);
+
+        // Set the behavior of the method validate
+        $this->validator->expects($this->once())
+            ->method('validate')
+            ->with($file, [])
+            ->willReturn(new ConstraintViolationList([
+                new ConstraintViolation('error', null, [], null, 'some', null),
+            ]));
+
+        $this->createResolver()->resolve($request, $meta)->next();
     }
 
     private function createResolver(): RequestFileArgumentResolver
