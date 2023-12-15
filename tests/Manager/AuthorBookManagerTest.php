4f580f9d16a8cac173422caa75792668032c8cc0
tarasmoroz <taras.moroz@ekreative.com>
2023-12-15 14:42:11 +0100

    Added test on remove file from cover of book

--- tests/Manager/AuthorBookManagerTest.php
+++ tests/Manager/AuthorBookManagerTest.php
@@ -15,6 +15,8 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class AuthorBookManagerTest extends AbstractTestCase
 {
+    const LINK_IMAGE_FILE = 'http://localhost/new.jpg';
+
     private BookRepository $bookRepository;
 
     private BookFormatRepository $bookFormatRepository;
@@ -59,10 +61,44 @@ class AuthorBookManagerTest extends AbstractTestCase
         $this->uploadFileManager->expects($this->once())
             ->method('uploadBookFile')
             ->with(1, $file)
-            ->willReturn('http://localhost/new.jpg');
+            ->willReturn(self::LINK_IMAGE_FILE);
 
         // Expected value
-        $expected = new UploadCoverResponse('http://localhost/new.jpg');
+        $expected = new UploadCoverResponse(self::LINK_IMAGE_FILE);
+
+        // Comparing the expected value with the actual returned value
+        $this->assertEquals($expected, $this->createManager()->uploadCover(1, $file));
+    }
+
+    public function testUploadCoverRemoveOld(): void
+    {
+        $file = new UploadedFile('path', 'field', null, UPLOAD_ERR_NO_FILE, true);
+        $book = (new Book())->setImage('http://localhost/old.png');
+        $this->setEntityId($book, 1);
+
+        // Set the behavior and return result for method - getBookById
+        $this->bookRepository->expects($this->once())
+            ->method('getBookById')
+            ->with(1)
+            ->willReturn($book);
+
+        // Set the behavior for method - commit
+        $this->bookRepository->expects($this->once())
+            ->method('commit');
+
+        // Set the behavior and return result for method - uploadBookFile
+        $this->uploadFileManager->expects($this->once())
+            ->method('uploadBookFile')
+            ->with(1, $file)
+            ->willReturn(self::LINK_IMAGE_FILE);
+
+        // Set the behavior and return result for method - deleteBookFile
+        $this->uploadFileManager->expects($this->once())
+            ->method('deleteBookFile')
+            ->with(1, 'old.png');
+
+        // Expected value
+        $expected = new UploadCoverResponse(self::LINK_IMAGE_FILE);
 
         // Comparing the expected value with the actual returned value
         $this->assertEquals($expected, $this->createManager()->uploadCover(1, $file));
