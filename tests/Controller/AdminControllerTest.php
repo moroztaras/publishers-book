4b82106baa7e99360c5cf7bca559a98b0d54634f
tarasmoroz <taras.moroz@ekreative.com>
2023-12-24 20:24:40 +0100

    Added testCreateCategory

--- tests/Controller/AdminControllerTest.php
+++ tests/Controller/AdminControllerTest.php
@@ -23,6 +23,32 @@ class AdminControllerTest extends AbstractControllerTest
         $this->assertResponseIsSuccessful();
     }
 
+    public function testCreateCategory(): void
+    {
+        // Create admin and auth
+        $this->createAdminAndAuth('user@test.com', 'testtest');
+
+        // Send request with request body
+        $this->client->request(Request::METHOD_POST, '/api/v1/admin/bookCategory', [], [], [], json_encode([
+            'title' => 'Test Chapter',
+        ]));
+
+        // Get response
+        $responseContent = json_decode($this->client->getResponse()->getContent());
+
+        // The request was successful.
+        $this->assertResponseIsSuccessful();
+
+        // Comparing the actual response content with the expected schema.
+        $this->assertJsonDocumentMatchesSchema($responseContent, [
+            'type' => 'object',
+            'required' => ['id'],
+            'properties' => [
+                'id' => ['type' => 'integer'],
+            ],
+        ]);
+    }
+
     public function testDeleteCategory(): void
     {
         // Create book category
