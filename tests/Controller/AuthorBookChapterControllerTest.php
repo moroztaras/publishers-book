55257bfa498f40ab87668e0f3178d342c5956a1f
tarasmoroz <taras.moroz@ekreative.com>
2023-12-25 21:09:24 +0100

    Added testUpdateBookChapter

--- tests/Controller/AuthorBookChapterControllerTest.php
+++ tests/Controller/AuthorBookChapterControllerTest.php
@@ -38,4 +38,26 @@ class AuthorBookChapterControllerTest extends AbstractControllerTest
             ],
         ]);
     }
+
+    public function testUpdateBookChapter(): void
+    {
+        // Create author and auth
+        $user = $this->createAuthorAndAuth('user@test.com', 'testtest');
+        // Create book
+        $book = MockUtils::createBook()->setUser($user);
+        // Create chapter
+        $chapter = MockUtils::createBookChapter($book);
+
+        // Save
+        $this->em->persist($book);
+        $this->em->persist($chapter);
+        $this->em->flush();
+
+        // Send request with body
+        $this->client->request(Request::METHOD_PUT, '/api/v1/author/book/'.$book->getId().'/chapter', [], [], [],
+            json_encode(['title' => 'Updated Book Chapter', 'id' => $chapter->getId()]));
+
+        // Response was successful
+        $this->assertResponseIsSuccessful();
+    }
 }
