f2714f6210d72c29ef7e482b9984477aa084b31a
tarasmoroz <taras.moroz@ekreative.com>
2023-12-25 11:48:12 +0100

    Added testUploadBookCover

--- tests/Controller/AuthorControllerTest.php
+++ tests/Controller/AuthorControllerTest.php
@@ -4,6 +4,9 @@ namespace App\Tests\Controller;
 
 use App\Tests\AbstractControllerTest;
 use Symfony\Component\HttpFoundation\Request;
+use App\Tests\MockUtils;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class AuthorControllerTest extends AbstractControllerTest
 {
@@ -31,4 +34,50 @@ class AuthorControllerTest extends AbstractControllerTest
             ],
         ]);
     }
+
+    public function testUploadBookCover(): void
+    {
+        // Create admin and auth
+        $user = $this->createAuthorAndAuth('user@test.com', 'testtest');
+        // Create book
+        $book = MockUtils::createBook()->setUser($user)->setImage(null);
+
+        // Save
+        $this->em->persist($book);
+        $this->em->flush();
+
+        $fixturePath = __DIR__.'/../Fixtures/book_cover.png';
+        $clonedImagePath = sys_get_temp_dir().PATH_SEPARATOR.'test.png';
+
+        (new Filesystem())->copy($fixturePath, $clonedImagePath);
+
+        $uploadedFile = new UploadedFile(
+            $clonedImagePath,
+            'test.png',
+            'image/png',
+            null,
+            true,
+        );
+
+        // Send request
+        $this->client->request(Request::METHOD_POST, '/api/v1/author/book/'.$book->getId().'/uploadCover', [], [
+            'cover' => $uploadedFile,
+        ]);
+
+        // Get response
+        $responseContent = json_decode($this->client->getResponse()->getContent());
+
+        // Response was successful
+        $this->assertResponseIsSuccessful();
+
+        // Comparing the actual response content with the expected schema.
+        $this->assertJsonDocumentMatchesSchema($responseContent, [
+            'type' => 'object',
+            'required' => ['link'],
+            'properties' => [
+                'link' => ['type' => 'string'],
+            ],
+        ]);
+    }
+
 }
