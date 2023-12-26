4fd49c873e6b03c3e3df9a472022ec3f2c76bbe5
tarasmoroz <taras.moroz@ekreative.com>
2023-12-26 16:34:59 +0100

    Added testCreateChapterNested

--- tests/Manager/AuthorBookChapterManagerTest.php
+++ tests/Manager/AuthorBookChapterManagerTest.php
@@ -8,10 +8,12 @@ use App\Exception\BookChapterInvalidSortException;
 use App\Manager\AuthorBookChapterManager;
 use App\Manager\BookChapterManager;
 use App\Model\Author\CreateBookChapterRequest;
+use App\Model\IdResponse;
 use App\Repository\BookChapterRepository;
 use App\Repository\BookRepository;
 use App\Tests\AbstractTestCase;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\String\UnicodeString;
 
 class AuthorBookChapterManagerTest extends AbstractTestCase
 {
@@ -59,6 +61,59 @@ class AuthorBookChapterManagerTest extends AbstractTestCase
         // Run Chapter manager
         $this->createManager()->createChapter($payload, 1);
     }
+
+    public function testCreateChapterNested(): void
+    {
+        $book = new Book();
+        $parentBookChapter = (new BookChapter())->setLevel(1);
+
+        $this->bookChapterRepository->expects($this->once())
+            ->method('getById')
+            ->with(2)
+            ->willReturn($parentBookChapter);
+
+        $this->slugger->expects($this->once())
+            ->method('slug')
+            ->with('test')
+            ->willReturn(new UnicodeString('test'));
+
+        $this->bookRepository->expects($this->once())
+            ->method('getBookById')
+            ->with(1)
+            ->willReturn($book);
+
+        $this->bookChapterRepository->expects($this->once())
+            ->method('getMaxSort')
+            ->with($book, 2)
+            ->willReturn(5);
+
+        $this->bookChapterRepository->expects($this->once())
+            ->method('saveAndCommit')
+            ->with($this->callback(function (BookChapter $chapter) use ($book, $parentBookChapter) {
+                $expectedChapter = (new BookChapter())
+                    ->setBook($book)
+                    ->setSort(6)
+                    ->setLevel(2)
+                    ->setTitle('test')
+                    ->setSlug('test')
+                    ->setParent($parentBookChapter);
+
+                $this->assertEquals($expectedChapter, $chapter);
+                $this->setEntityId($chapter, 1);
+
+                return true;
+            }));
+
+        // Create request
+        $payload = (new CreateBookChapterRequest())->setTitle('test')->setParentId(2);
+
+        // Comparing the expected value with the actual returned value
+        $this->assertEquals(
+            new IdResponse(1),
+            $this->createManager()->createChapter($payload, 1),
+        );
+    }
+
 
     // Create AuthorBookChapterManager
     private function createManager(): AuthorBookChapterManager
