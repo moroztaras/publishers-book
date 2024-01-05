f0d703c962a2ccbd18333ab9d7d94a2ffc3156aa
tarasmoroz <taras.moroz@ekreative.com>
2024-01-05 14:57:48 +0100

    Added api link for create new content of book chapter

---
+++ src/Controller/AuthorBookChapterContentController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Attribute\RequestBody;
+use App\Exception\BookChapterNotFoundException;
+use App\Manager\BookContentManager;
+use App\Model\Author\CreateBookChapterContentRequest;
+use App\Model\ErrorResponse;
+use App\Model\IdResponse;
+use App\Security\Voter\AuthorBookVoter;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class AuthorBookChapterContentController extends AbstractController
+{
+    public function __construct(private readonly BookContentManager $bookContentManager)
+    {
+    }
+
+    // TODO Need add 'BookChapterNotFoundException' to response 404
+    #[Route(path: '/api/v1/author/book/{bookId}/chapter/{chapterId}/content', methods: ['POST'])]
+    #[IsGranted(AuthorBookVoter::IS_AUTHOR, subject: 'bookId')]
+    #[OA\Tag(name: 'Author book chapter content API')]
+    #[OA\Response(response: 200, description: 'Create a book chapter content', attachables: [new Model(type: IdResponse::class)])]
+    #[OA\Response(response: 400, description: 'Validation failed', attachables: [new Model(type: ErrorResponse::class)])]
+    #[OA\Response(response: 404, description: 'Book chapter not found')]
+    #[OA\RequestBody(attachables: [new Model(type: CreateBookChapterContentRequest::class)])]
+    public function createBookChapterContent(#[RequestBody] CreateBookChapterContentRequest $request, int $bookId, int $chapterId): Response
+    {
+        return $this->json($this->bookContentManager->createContent($request, $chapterId));
+    }
+}
---
+++ src/Manager/BookContentManager.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Manager;
+
+use App\Entity\BookContent;
+use App\Model\Author\CreateBookChapterContentRequest;
+use App\Model\IdResponse;
+use App\Repository\BookChapterRepository;
+use App\Repository\BookContentRepository;
+
+class BookContentManager
+{
+    public function __construct(
+        private readonly BookContentRepository $bookContentRepository,
+        private readonly BookChapterRepository $bookChapterRepository)
+    {
+    }
+
+    public function createContent(CreateBookChapterContentRequest $request, int $chapterId): IdResponse
+    {
+        $content = (new BookContent())->setChapter($this->bookChapterRepository->getById($chapterId));
+
+        $this->saveContent($request, $content);
+
+        return new IdResponse($content->getId());
+    }
+
+    private function saveContent(CreateBookChapterContentRequest $request, BookContent $content): void
+    {
+        $content->setContent($request->getContent());
+        $content->setIsPublished($request->isPublished());
+
+        $this->bookContentRepository->saveAndCommit($content);
+    }
+}
