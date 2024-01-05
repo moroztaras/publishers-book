e10f07811f03524086ab6a576614c59562da6952
tarasmoroz <taras.moroz@ekreative.com>
2024-01-05 12:58:17 +0100

    Added new entity BookContent and BookContentRepository and Migration version.

---
+++ src/Repository/BookContentRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\BookContent;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method BookContent|null find($id, $lockMode = null, $lockVersion = null)
+ * @method BookContent|null findOneBy(array $criteria, array $orderBy = null)
+ * @method BookContent[]    findAll()
+ * @method BookContent[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class BookContentRepository extends ServiceEntityRepository
+{
+    use RepositoryModifyTrait;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, BookContent::class);
+    }
+}
