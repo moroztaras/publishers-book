5cad80f12f0b90e54ee2ffd56c46b7c4ea774250
tarasmoroz <taras.moroz@ekreative.com>
2024-01-07 21:21:00 +0100

    Added testUpdateBookContent

--- tests/Controller/AuthorBookChapterContentControllerTest.php
+++ tests/Controller/AuthorBookChapterContentControllerTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Controller;
 use App\Tests\AbstractControllerTest;
 use App\Tests\MockUtils;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthorBookChapterContentControllerTest extends AbstractControllerTest
 {
@@ -23,7 +24,7 @@ class AuthorBookChapterContentControllerTest extends AbstractControllerTest
         $this->em->flush();
 
         $url = sprintf('/api/v1/author/book/%d/chapter/%d/content', $book->getId(), $chapter->getId());
-        $requestContent = json_encode(['content' => 'Test Content', 'published' => true]);
+        $requestContent = json_encode(['content' => 'New Test Content', 'published' => true]);
 
         $this->client->request(Request::METHOD_POST, $url, [], [], [], $requestContent);
 
@@ -44,5 +45,35 @@ class AuthorBookChapterContentControllerTest extends AbstractControllerTest
                 'id' => ['type' => 'integer'],
             ],
         ]);
+    }
+
+    public function testUpdateBookContent(): void
+    {
+        // Create user
+        $user = $this->createAuthorAndAuth('user@test.com', 'testtest');
+        // Create book
+        $book = MockUtils::createBook()->setUser($user);
+        // Create chapter
+        $chapter = MockUtils::createBookChapter($book);
+        // Create content
+        $content = MockUtils::createBookContent($chapter);
+
+        // Save
+        $this->em->persist($book);
+        $this->em->persist($chapter);
+        $this->em->persist($content);
+        $this->em->flush();
+
+        $url = sprintf('/api/v1/author/book/%d/chapter/%d/content/%d', $book->getId(), $chapter->getId(), $content->getId());
+        $requestContent = json_encode(['content' => 'Edit Test Content', 'published' => false]);
+
+        // Send request
+        $this->client->request(Request::METHOD_PUT, $url, [], [], [], $requestContent);
+
+        // Response was successful
+        $this->assertResponseIsSuccessful();
+
+        // Comparing the expected value with the actual returned value.
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
     }
 }
