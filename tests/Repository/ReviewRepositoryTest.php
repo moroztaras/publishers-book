665104832cda9ca25f848f97fc2edff152539fa4
tarasmoroz <taras.moroz@ekreative.com>
2024-02-03 20:23:28 +0100

    Added testGetBookTotalRatingSum

--- tests/Repository/ReviewRepositoryTest.php
+++ tests/Repository/ReviewRepositoryTest.php
@@ -36,4 +36,25 @@ class ReviewRepositoryTest extends AbstractTestRepository
         $this->assertIsNumeric($this->reviewRepository->countByBookId($book->getId()));
         $this->assertEquals(1, $this->reviewRepository->countByBookId($book->getId()));
     }
+
+    public function testGetBookTotalRatingSum()
+    {
+        // Create user
+        $user = MockUtils::createUser();
+        $this->em->persist($user);
+        // Create book
+        $book = MockUtils::createBook()->setUser($user);
+        $this->em->persist($book);
+        // Create Reviews
+        $this->em->persist(MockUtils::createReview($book));
+        $this->em->persist(MockUtils::createReview($book)->setRating(4));
+        $this->em->persist(MockUtils::createReview($book)->setRating(3));
+
+        // Save
+        $this->em->flush();
+
+        // Comparing the expected value with the actual returned value.
+        $this->assertIsNumeric($this->reviewRepository->getBookTotalRatingSum($book->getId()));
+        $this->assertEquals((int) 12, (int) $this->reviewRepository->getBookTotalRatingSum($book->getId()));
+    }
 }
